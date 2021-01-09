72fe78ec340a1fa9e816ad94bc7cb9d661292319
Sabri <Sabri.Al-Safi@selectedinterventions.com>
2021-01-09 20:58:18 +0000

    Day15

---
+++ Day15/part2.py
@@ -0,0 +1,11 @@
+starts = [11,18,0,20,1,7,16]
+last = {}
+prev = -1
+
+for n in range(30000000):
+    cur = starts[n] if n < len(starts) else \
+          n - last[prev] if prev in last else \
+          0
+    last[prev] = n
+    prev = cur
+print(cur)
